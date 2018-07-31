1c567d9ad1bc15a1a1900a6b23a238efa4d0fcbb
Eric Butterfield <ewbutterfield@gmail.com>
2018-07-31 14:00:11 -0600

    add some starting points

--- main.js
+++ main.js
@@ -2,3 +2,5 @@
 // Install any library you need to do so.
 
 // HINT! http://pokeapi.co/api/v2/pokemon?limit=20&offset=20 will give you a list of the first 20 pokemon
+
+console.log("Let's do this!");
